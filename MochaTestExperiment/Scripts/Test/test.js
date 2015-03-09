12eb0f5be8d93c35da575238541c92fd48bad3af
Matthew Nichols <matthew@matthew-nichols.com>
2015-03-09 10:07:39 -0600

    Adding project files.

---
+++ MochaTestExperiment/Scripts/Test/test.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../require.js" />
+/// <reference path="../chai.js" />
+
+var assert = require("assert");
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+            assert.equal(-1, [1, 2, 3].indexOf(0));
+        });
+    });
+});
