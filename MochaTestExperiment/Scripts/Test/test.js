5fa9b807a3490b9166e8fe2de69558a017fbcec0
Matthew Nichols <matthew@matthew-nichols.com>
2015-03-09 19:49:56 -0600

    Got a single test running in both Node and Chutzpah

--- MochaTestExperiment/Scripts/Test/test.js
+++ MochaTestExperiment/Scripts/Test/test.js
@@ -1,10 +1,16 @@
-﻿/// <reference path="../require.js" />
-/// <reference path="../chai.js" />
+﻿
+//var assert;
+//if (typeof chai !== "undefined") {
+//    assert = chai.assert;
+//} else {
+//    assert = require("chai").assert;
+//}
 
-var assert = require("assert");
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+var assert = chai.assert;
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(5));
             assert.equal(-1, [1, 2, 3].indexOf(0));
         });
